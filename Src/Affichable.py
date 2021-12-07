55ab3f7ce97566c48091f662ca10746b2b1665eb
Nevens-fr <aurelientudo@hotmail.fr>
2021-12-07 11:29:23 +0100

    Modif useless args

--- Src/Affichable.py
+++ Src/Affichable.py
@@ -1,21 +1,18 @@
 import Draw
 
 #Classe abstraite qui permet de définir des comportemens par défaut 
-#à toutes les classes en héritant
+#à toutes les classes en héritant pour afficher des images
 class affichable:
+
     #constructeur
     #coords : tuple des coordonnées ou placer l'élément à l'écran
     #path : chemin relatif vers l'image à afficher
-    def __init__(self, coords, path, color):
+    def __init__(self, coords, path ):
         self.img = Draw.createImg(path)
         self.coords = coords
-        self.color = color
-        
-    def getColor(self):
-        return self.color
 
     def getImg(self):
         return self.img
-        
+
     def getCoords(self):
         return self.coords
--- Src/Bouton.py
+++ Src/Bouton.py
@@ -3,8 +3,10 @@ import Draw
 
 class Bouton(Affichable.affichable):
 
-    def __init__(self, coords, path, color):
-        super().__init__(coords, path, color)
+    #Constructeur
+    def __init__(self, coords, path):
+        super().__init__(coords, path)
 
+    #Affichage
     def afficher(self, screen):
         Draw.drawBlit(screen, self)
